0b6f7c758d42e52c349f9c4a362b26549bcb6e2b
kompilator69 <marcinorz@o2.pl>
2020-04-29 15:03:31 +0200

    a

--- PS8/Models/Product.cs
+++ PS8/Models/Product.cs
@@ -9,10 +9,13 @@ namespace PS8.Models
     public class Product
     {
         [Display(Name = "Id")]
+        [Required]
         public int id { get; set; }
         [Display(Name = "Nazwa produktu")]
+        [Required]
         public string name { get; set; }
         [Display(Name = "Cena")]
+        [Required]
         public decimal price { get; set; }
 
     }
